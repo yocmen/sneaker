3aecf907656ee331557a030e2f33a864f5ca4bb5
Yocsel Mendoza <yocmen@gmail.com>
2017-06-20 17:54:06 -0400

    Add commands

--- src/CssInlineMailer.php
+++ src/CssInlineMailer.php
@@ -48,7 +48,7 @@ class CssInlineMailer
         $content = $this->convertCssToInlineStyles($content);
 
         //$this->mailer->queue('sneaker::raw', compact('content'), $callback);
-        Mail::send(compact('content'), $callback);
+        Mail::raw(compact('content'), $callback);
     }
 
     /**
