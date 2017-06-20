38b13bb994d0c31587218528250afcc2c9e86d04
Yocsel Mendoza <yocmen@gmail.com>
2017-06-20 16:57:41 -0400

    Add commands

---
+++ src/Exceptions/DummyException.php
@@ -0,0 +1,10 @@
+<?php
+namespace SquareBoat\Sneaker\Exceptions;
+use Exception;
+class DummyException extends Exception
+{
+    /**
+     * @var string
+     */
+    protected $message = 'The dummy exception.';
+}
